ae53fb9e3cdb8b62ec5972afab0004d8de522eda
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-12-12 23:46:47 +0800

    feat: 给 LinkStack 类及其节点类添加 Doxygen 风格注释

--- include/LinkStack.h
+++ include/LinkStack.h
@@ -7,25 +7,37 @@
 
 namespace datastructures
 {
-// 链栈的节点类定义
+/**
+ * @brief 链栈的节点类定义
+ * @tparam elemType 链栈中存储的数据类型
+ */
 template <class elemType>
 class LinkStack;  // 前向声明 LinkStack 模板类
 
-// 链栈的节点类定义
+/**
+ * @brief 链栈的节点类
+ * @tparam elemType 链栈中存储的数据类型
+ */
 template <class elemType>
 class Node
 {
     friend class LinkStack<elemType>;
 
    private:
-    elemType data;         // 存储数据
-    Node<elemType> *next;  // 指向下一个节点
+    elemType data;         ///< 存储节点数据
+    Node<elemType> *next;  ///< 指向下一个节点的指针
 
    public:
-    // 默认构造函数
+    /**
+     * @brief 默认构造函数
+     */
     Node() { next = nullptr; }
 
-    // 带参数构造函数
+    /**
+     * @brief 带参数构造函数
+     * @param x 节点存储的数据
+     * @param p 指向下一个节点的指针（默认为 nullptr）
+     */
     Node(const elemType &x, Node<elemType> *p = nullptr)
     {
         data = x;
@@ -33,37 +45,65 @@ class Node
     }
 };
 
-// 链栈类定义
+/**
+ * @brief 链栈类
+ * @tparam elemType 链栈中存储的数据类型
+ */
 template <class elemType>
 class LinkStack
 {
    private:
-    Node<elemType> *top_p;  // 栈顶指针
+    Node<elemType> *top_p;  ///< 栈顶指针
 
    public:
-    // 默认构造函数，初始化栈为空
+    /**
+     * @brief 默认构造函数，初始化栈为空
+     */
     LinkStack() { top_p = nullptr; }
 
-    // 判断栈是否为空
+    /**
+     * @brief 判断栈是否为空
+     * @return 如果栈为空返回 true，否则返回 false
+     */
     bool isEmpty() { return top_p == nullptr; }
 
-    // 判断栈是否已满（对于链栈永远不会满）
+    /**
+     * @brief 判断栈是否已满（对于链栈，永远不会满）
+     * @return 返回 false
+     */
     bool isFull() { return false; }
 
-    // 返回栈顶元素
+    /**
+     * @brief 返回栈顶元素
+     * @return 栈顶元素的值
+     * @throw OutOfBound 如果栈为空，抛出 OutOfBound 异常
+     */
     elemType top();
 
-    // 入栈操作
+    /**
+     * @brief 入栈操作
+     * @param e 要入栈的元素
+     */
     void push(const elemType &e);
 
-    // 出栈操作
+    /**
+     * @brief 出栈操作
+     * @throw OutOfBound 如果栈为空，抛出 OutOfBound 异常
+     */
     void pop();
 
-    // 析构函数，清理栈内所有节点
+    /**
+     * @brief 析构函数，清理栈内所有节点
+     */
     ~LinkStack();
 };
 
-// 返回栈顶元素
+/**
+ * @brief 返回栈顶元素
+ * @tparam elemType 链栈中存储的数据类型
+ * @return 栈顶元素的值
+ * @throw OutOfBound 如果栈为空，抛出 OutOfBound 异常
+ */
 template <class elemType>
 elemType LinkStack<elemType>::top()
 {
@@ -74,7 +114,11 @@ elemType LinkStack<elemType>::top()
     return top_p->data;
 }
 
-// 出栈操作
+/**
+ * @brief 出栈操作
+ * @tparam elemType 链栈中存储的数据类型
+ * @throw OutOfBound 如果栈为空，抛出 OutOfBound 异常
+ */
 template <class elemType>
 void LinkStack<elemType>::pop()
 {
@@ -87,14 +131,21 @@ void LinkStack<elemType>::pop()
     delete tmp;           // 删除原栈顶节点
 }
 
-// 入栈操作
+/**
+ * @brief 入栈操作
+ * @tparam elemType 链栈中存储的数据类型
+ * @param e 要入栈的元素
+ */
 template <class elemType>
 void LinkStack<elemType>::push(const elemType &e)
 {
     top_p = new Node<elemType>(e, top_p);  // 新节点插入栈顶
 }
 
-// 析构函数，清理栈内的所有节点
+/**
+ * @brief 析构函数，清理栈内的所有节点
+ * @tparam elemType 链栈中存储的数据类型
+ */
 template <class elemType>
 LinkStack<elemType>::~LinkStack()
 {
