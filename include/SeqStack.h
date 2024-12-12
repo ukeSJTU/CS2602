87ace938df3935e9b91ebc1f4fc70030b5adaa04
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-12-12 23:35:32 +0800

    chore: 给SeqStack添加了doxygen风格注释

--- include/SeqStack.h
+++ include/SeqStack.h
@@ -6,23 +6,79 @@
 namespace datastructures
 {
 
+/**
+ * @brief 顺序栈类模板
+ *
+ * 使用动态数组实现的顺序栈，支持自动扩容。
+ *
+ * @tparam elemType 栈中元素类型
+ */
 template <class elemType>
 class SeqStack
 {
    private:
-    elemType *elem;
-    int top_p;  // index of the top element
-    int maxSize;
-    void doubleSpace();
+    elemType *elem;      ///< 栈存储数组，存放实际的数据元素
+    int top_p;           ///< 栈顶下标
+    int maxSize;         ///< 当前数组的最大容量
+    void doubleSpace();  ///< 将存储空间扩容为原来的两倍
 
    public:
+    /**
+     * @brief 构造函数
+     *
+     * 初始化一个给定容量的顺序栈。
+     *
+     * @param initSize 栈的初始容量，默认为100
+     * @exception IllegalSize 如果分配内存失败则抛出该异常
+     */
     SeqStack(int initSize = 100);
-    bool isEmpty() { return (top_p == -1); };
-    bool isFull() { return (top_p == maxSize - 1); };
+
+    /**
+     * @brief 判断栈是否为空
+     *
+     * @return 若栈为空返回true，否则返回false
+     */
+    bool isEmpty() { return (top_p == -1); }
+
+    /**
+     * @brief 判断栈是否已满
+     *
+     * @return 若栈已满返回true，否则返回false
+     */
+    bool isFull() { return (top_p == maxSize - 1); }
+
+    /**
+     * @brief 获取栈顶元素
+     *
+     * @return 返回栈顶元素的拷贝
+     * @exception OutOfBound 如果栈为空则抛出该异常
+     */
     elemType top();
+
+    /**
+     * @brief 向栈顶压入新元素
+     *
+     * 如果栈已满则自动扩容。
+     *
+     * @param e 要入栈的元素
+     */
     void push(const elemType &e);
+
+    /**
+     * @brief 弹出栈顶元素
+     *
+     * 从栈中移除并丢弃栈顶元素。
+     *
+     * @exception OutOfBound 如果栈为空则抛出该异常
+     */
     void pop();
-    ~SeqStack() { delete[] elem; };
+
+    /**
+     * @brief 析构函数
+     *
+     * 释放栈内存。
+     */
+    ~SeqStack() { delete[] elem; }
 };
 
 template <class elemType>
@@ -37,6 +93,13 @@ SeqStack<elemType>::SeqStack(int initSize)
     top_p = -1;
 }
 
+/**
+ * @brief 将栈的存储空间扩容一倍
+ *
+ * 分配两倍容量的新数组，将原有数据拷贝到新数组中，然后释放旧数组。
+ *
+ * @exception IllegalSize 如果内存分配失败则抛出该异常
+ */
 template <class elemType>
 void SeqStack<elemType>::doubleSpace()
 {
